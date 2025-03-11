f1dba509cc18fefe23af9b6616f3a1cc3fdc4b4a
Mati Joseph <crucialniccur@gmail.com>
2025-03-11 06:01:06 +0300

    define findMatching function

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+function findMatching(collection, driverName) {
+  return collection.filter(function (driver) {
+    return driver.toLowerCase() === driverName.toLowerCase();
+  });
+}
+
+console.log(findMatching(drivers, "Sammy"));
+console.log(findMatching(drivers, "Bobby"));
